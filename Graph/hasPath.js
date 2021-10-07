fdc1ce8286a7d722e7188b276312e7ff749ecfba
JBayona <jordavids_22@hotmail.com>
2021-10-06 23:58:20 -0400

    Has Path BFS.

--- Graph/hasPath.js
+++ Graph/hasPath.js
@@ -17,15 +17,33 @@ const hasPathDFS = (graph, src, dst) => {
   return false;
 }
 
-const graph = {
-  f: ['g', 'i'],
-  g: ['h'],
-  h: [],
-  i: ['g', 'k'],
-  j: ['i'],
-  k: []
-}
+// BFS
+const hasPathBFS = (graph, src, dst) => {
+  let queue = [src];
+  while (queue.length) {
+    let size = queue.length;
+    for (let i = 0; i < size; i++) {
+      let node = queue.shift();
+      if (node === dst) {
+        return true;
+      }
+      for (let neighbor of graph[node]) {
+        queue.push(neighbor);
+      }
+    }
+  }
+  return false;
+};
 
-const src = 'f';
-const dst = 'k';
+const graph = {
+  f: ["g", "i"],
+  g: ["h"],
+  h: [],
+  i: ["g", "k"],
+  j: ["i"],
+  k: [],
+};
+const src = "f";
+const dst = "k";
 console.log(hasPathDFS(graph, src, dst));
+console.log(hasPathBFS(graph, src, dst));
