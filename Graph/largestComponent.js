2310cc0c17ac3bdf3a2a99d6c8a31f6f446a322b
JBayona <jordavids_22@hotmail.com>
2021-10-13 22:12:01 -0400

    BFS Largest Component.

--- Graph/largestComponent.js
+++ Graph/largestComponent.js
@@ -13,11 +13,11 @@ Find the largest component
 }
 */
 
-const largestComponentDFS = graph => {
+const largestComponentDFS = (graph) => {
   let visited = new Set();
   let size = 0;
   let result = 0;
-  for(let node in graph) {
+  for (let node in graph) {
     size = exploreDFS(graph, node, visited);
     result = Math.max(result, size);
   }
@@ -26,25 +26,62 @@ const largestComponentDFS = graph => {
 
 const exploreDFS = (graph, node, visited) => {
   // If the node has been visited already
-  if(visited.has(node)) {
+  if (visited.has(node)) {
     return 0;
   }
   visited.add(node);
   // Our current node counts as 1 for the visited elements
   let size = 1;
-  for(let neighbor of graph[node]) {
+  for (let neighbor of graph[node]) {
     size += exploreDFS(graph, neighbor, visited);
   }
   return size;
 };
 
+const largestComponentBFS = (graph) => {
+  let visited = new Set();
+  let result = 0;
+
+  for (let node in graph) {
+    if (!visited.has(node)) {
+      let size = exploreBFS(node, graph, visited);
+      result = Math.max(result, size);
+    }
+  }
+  return result;
+};
+
+const exploreBFS = (node, graph, visited) => {
+  let queue = [];
+  queue.push(node);
+  let largest = 0;
+  let count = 0;
+  while (queue.length) {
+    let size = queue.length;
+    for (let i = 0; i < size; i++) {
+      let node = queue.shift();
+      if (visited.has(node)) {
+        continue;
+      }
+      count++;
+      largest = Math.max(count, largest);
+      visited.add(node);
+      for (let neighbors of graph[node]) {
+        queue.push(neighbors);
+      }
+    }
+  }
+  return largest;
+};
+
 const graph = {
-  0: ['8', '1', '5'],
-  1: ['0'],
-  5: ['0', '8'],
-  8: ['0', '5'],
-  2: ['3', '4'],
-  3: ['2', '4'],
-  4: ['3', '2']
+  0: ["8", "1", "5"],
+  1: ["0"],
+  5: ["0", "8"],
+  8: ["0", "5"],
+  2: ["3", "4"],
+  3: ["2", "4"],
+  4: ["3", "2"],
 };
 console.log(largestComponentDFS(graph));
+console.log(largestComponentBFS(graph));
