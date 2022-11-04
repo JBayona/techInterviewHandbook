a3e0ba4a44ceaf9baa3f564673ef18fe5b0f4f59
JBayona <jordavids_22@hotmail.com>
2022-11-03 20:37:03 -0600

    Traversal DFS Iterative and Recursive.

--- Python/Graph/traversal.py
+++ Python/Graph/traversal.py
@@ -5,6 +5,11 @@ def depth_first_print(graph, source):
         print(current)
         for neighbor in graph[current]:
             stack.append(neighbor)
+            
+def depth_first_recursive_print(graph, source):
+    print(source)
+    for neighbor in graph[source]:
+        depth_first_recursive_print(graph, neighbor)
 
 graph = {
   'a': ['b', 'c'],
@@ -18,5 +23,7 @@ graph = {
 # Graph and the starting point
 print('DFS');
 depth_first_print(graph, 'a');
+print('DFS Recursive');
+depth_first_recursive_print(graph, 'a');
 #console.log('BFS');
 #breadthFirstPrint(graph, 'a')
