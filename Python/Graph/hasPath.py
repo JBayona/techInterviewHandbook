de97b8852620c3a53cd404054b6a423552f9ac36
JBayona <jordavids_22@hotmail.com>
2022-11-03 21:18:13 -0600

    Has Path BFS.

--- Python/Graph/hasPath.py
+++ Python/Graph/hasPath.py
@@ -1,9 +1,23 @@
 def has_path(graph, source, dst):
     if source == dst:
-        return true
+        return True
     
     for neighbor in graph[source]:
         if has_path(graph, neighbor, dst):
-            return true
+            return True
     
-    return false
+    return False
+
+
+def has_path_bfs(graph, source, dst):
+    queue = [source]
+    
+    while len(queue)> 0:
+        current = queue.pop(0)
+        if current == dst:
+            return True
+        
+        for neighbor in graph[current]:
+            queue.append(neighbor)
+    
+    return False
