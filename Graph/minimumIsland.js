02e1ba03c0c5df9901baf55cd0c7f226f2d62cc5
JBayona <jordavids_22@hotmail.com>
2022-11-07 19:56:02 -0600

    Minimum Island.

--- Graph/minimumIsland.js
+++ Graph/minimumIsland.js
@@ -53,6 +53,54 @@ const explore = (grid, visited, row, col) => {
   return count;
 }
 
+// Option 2
+// const minimumIsland = (grid) => {
+//   let ROW = grid.length;
+//   let COL = grid[0].length;
+//   let visited = new Array(ROW);
+//   for(let i = 0; i < ROW; i++) {
+//     visited[i] = new Array(COL).fill(0);
+//   }
+  
+//   let min = Number.MAX_SAFE_INTEGER;
+//   let result = Number.MAX_SAFE_INTEGER;
+//   for(let i = 0; i < ROW; i++) {
+//     for(let j = 0; j < COL; j++) {
+//       if(grid[i][j] && !visited[i][j]) {
+//         visited[i][j] = 1;
+//         min = dfs(grid, visited, i, j);
+//         result = Math.min(min, result)
+//       }
+//     }
+//   }
+//   return result;
+// };
+// const dfs = (grid, visited, row, col) => {
+//   let ROW = [0, -1, 0, 1];
+//   let COL = [-1, 0, 1, 0];
+
+//   let sum = 1;
+//   for(let i = 0; i < 4; i++) {
+//     let newRow = ROW[i] + row;
+//     let newCol = COL[i] + col;
+//     if(isSafe(grid, visited, newRow, newCol)) {
+//       visited[newRow][newCol] = 1;
+//       sum += dfs(grid, visited, newRow, newCol);
+//     }
+//   }
+//   return sum;
+// }
+// const isSafe = (grid, visited, row, col) => {
+//   let ROW = grid.length;
+//   let COL = grid[0].length;
+//   return (
+//     row >= 0 && row < ROW &&
+//     col >= 0 && col < COL &&
+//     grid[row][col] &&
+//     !visited[row][col]
+//   );
+// }
+
 const grid = [
   [0, 1, 0, 0, 1, 0],
   [1, 1, 0, 0, 1, 0],
